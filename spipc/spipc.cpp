99c82b9c97df187a310ed3f1e6dca831758b07df
kr0st <kr0st@bioreactor.me>
2014-03-22 07:36:32 +0200

    Extended mem transport test.

--- spipc/spipc.cpp
+++ spipc/spipc.cpp
@@ -67,11 +67,17 @@ size_t Memory_Transport::read(void* buf, size_t buf_size, size_t timeout)
     return read_bytes;
 }
 
+std::recursive_mutex g_test_mutex;
+std::vector<std::string> g_written_items;
+
 void th1_mem_trans_test(Memory_Transport* trans)
 {
     static unsigned counter = 0;
-    
-    while (counter++ != 10)
+    srand(std::this_thread::get_id().hash());
+
+    std::vector<std::string> written_items;
+
+    while (counter++ < 10)
     {
         int rnd_sz = 1 + rand() % 12;
     
@@ -84,28 +90,41 @@ void th1_mem_trans_test(Memory_Transport* trans)
         printf("Writing: %s (%d bytes)\n", write_buf, rnd_sz + 1);
 
         trans->write(write_buf, rnd_sz + 1);
+        written_items.push_back(write_buf);
 
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        //std::this_thread::sleep_for(std::chrono::seconds(1));
     }
+
+    std::lock_guard<std::recursive_mutex> lock(g_test_mutex);
+    g_written_items.insert(g_written_items.end(), written_items.begin(), written_items.end());
 }
+
+std::vector<std::string> g_read_items;
 
 void th2_mem_trans_test(Memory_Transport* trans)
 {
     static unsigned counter = 0;
+    srand(std::this_thread::get_id().hash());
     
-    while (counter++ != 10)
+    std::vector<std::string> read_items;
+
+    while (counter++ < 10)
     {
         char read_buf[256];
         memset(read_buf, 0, sizeof(read_buf));
         trans->read(read_buf, sizeof(read_buf));
 
+        read_items.push_back(read_buf);
         printf("Reading: %s\n", read_buf);
 
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        //std::this_thread::sleep_for(std::chrono::seconds(1));
     }
+
+    std::lock_guard<std::recursive_mutex> lock(g_test_mutex);
+    g_read_items.insert(g_read_items.end(), read_items.begin(), read_items.end());
 }
 
-bool two_threads_mem_transport_test()
+bool N_threads_mem_transport_test()
 {
     Memory_Transport trans;
 
@@ -115,13 +134,77 @@ bool two_threads_mem_transport_test()
     writer.join();
     reader.join();
 
+    if (g_read_items.size() != g_written_items.size())
+    {
+        printf("ERROR: number of read items != number of written items.");
+        return false;
+    }
+
+    if (g_read_items.size() == 0)
+    {
+        printf("ERROR: number of read items is 0.");
+        return false;
+    }
+
+    for (std::vector<std::string>::iterator i = g_written_items.begin(); i != g_written_items.end();)
+    {
+        for (std::vector<std::string>::iterator j = g_read_items.begin(); j != g_read_items.end();)
+        {
+            if (*i == *j)
+            {
+                g_written_items.erase(i);
+                g_read_items.erase(j);
+
+                i = g_written_items.begin();
+                j = g_read_items.begin();
+            }
+            else
+            {
+                ++i;
+                ++j;
+            }
+        }
+    }
+
+    if (g_read_items.size() != g_written_items.size())
+    {
+        printf("ERROR: there was a corruption during read/write because not all written items were read.\n");
+        
+        printf("Leftovers in g_read_items:\n");
+        for (size_t i = 0; i < g_read_items.size(); ++i)
+            printf("%s ", g_read_items[i].c_str());
+
+        printf("\nLeftovers in g_written_items:\n");
+        for (size_t i = 0; i < g_written_items.size(); ++i)
+            printf("%s ", g_written_items[i].c_str());
+        printf("\n");
+
+        return false;
+    }
+
+    if (g_read_items.size() != 0)
+    {
+        printf("ERROR: read/write problem detected.\n");
+
+        printf("Leftovers in g_read_items:\n");
+        for (size_t i = 0; i < g_read_items.size(); ++i)
+            printf("%s ", g_read_items[i].c_str());
+
+        printf("\nLeftovers in g_written_items:\n");
+        for (size_t i = 0; i < g_written_items.size(); ++i)
+            printf("%s ", g_written_items[i].c_str());
+        printf("\n");
+
+        return false;
+    }
+
     return true;
 }
 
 void run_all_tests()
 {
-    if (!two_threads_mem_transport_test())
-        printf("two_threads_mem_transport_test failed.\n");
+    if (!N_threads_mem_transport_test())
+        printf("N_threads_mem_transport_test failed.\n");
         
     printf("tests finished OK.\n");
 }
