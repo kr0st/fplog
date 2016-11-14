5bf1e0181a3fbc2e9054cc73fb8ecdb1389b5bcf
Rostislav Kuratch <kr0st@bioreactor.me>
2016-11-14 21:56:04 +0200

    fplog_test project added to OSX build.

--- fplog/fplog_test.cpp
+++ fplog/fplog_test.cpp
@@ -1213,9 +1213,9 @@ bool socket_test()
 
 int main()
 {
-    fplog::testing::queue_controller_test();
+    //fplog::testing::queue_controller_test();
     
-    //fplog::testing::run_all_tests();
+    fplog::testing::run_all_tests();
 
     //fplog::testing::manual_test();
     
