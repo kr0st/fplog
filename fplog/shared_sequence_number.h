149b500a3bc0785a9f19b252628db5b277976934
kr0st <kr0st@bioreactor.me>
2017-02-05 19:18:09 +0200

    Fixed libsequence exports.

--- fplog/shared_sequence_number.h
+++ fplog/shared_sequence_number.h
@@ -3,9 +3,27 @@
 #include <fplog_exceptions.h>
 #include <mutex>
 
+#ifdef SEQUENCE_EXPORT
+
+#ifdef _LINUX
+#define SEQUENCE_API __attribute__ ((visibility("default")))
+#else
+#define SEQUENCE_API __declspec(dllexport)
+#endif
+
+#else
+
+#ifdef _LINUX
+#define SEQUENCE_API
+#else
+#define SEQUENCE_API __declspec(dllimport)
+#endif
+
+#endif
+
 namespace fplog {
 
-class Shared_Sequence_Number
+class SEQUENCE_API Shared_Sequence_Number
 {
     public:
 
