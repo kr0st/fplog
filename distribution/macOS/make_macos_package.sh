e499f7fc0067419d171ab1c286d742d8668bf5ce
kr0st <kr0st@bioreactor.me>
2017-02-23 16:01:11 +0200

    macOS distribution script relocated, fplog_testapp added.

---
+++ distribution/macOS/make_macos_package.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+rm -rf ../../Stage/fplog.framework
+mkdir ../../Stage/fplog.framework
+mkdir ../../Stage/fplog.framework/Versions
+mkdir ../../Stage/fplog.framework/Versions/A
+mkdir ../../Stage/fplog.framework/Versions/A/Resources
+mkdir ../../Stage/fplog.framework/Versions/A/Headers
+
+cp -r ../../Stage/*dylib ../../Stage/fplog.framework/Versions/A/
+cp -r ../../Stage/json.lua ../../Stage/fplog.framework/Versions/A/
+cp ../../fplog/Info.plist ../../Stage/fplog.framework/Versions/A/Resources/
+
+cp -r ../../common/fplog_exceptions.h ../../Stage/fplog.framework/Versions/A/Headers/
+cp -r ../../common/fplog_transport.h ../../Stage/fplog.framework/Versions/A/Headers/
+cp -r ../../fplog/fplog.h ../../Stage/fplog.framework/Versions/A/Headers/
+mkdir ../../Stage/fplog.framework/Versions/A/Headers/libjson
+cp -r ../../libjson/* ../../Stage/fplog.framework/Versions/A/Headers/libjson
+
+
+ln -s A ../../Stage/fplog.framework/Versions/Current
+ln -s Versions/Current/libfplog.dylib ../../Stage/fplog.framework/fplog
+ln -s Versions/Current/Headers ../../Stage/fplog.framework/Headers
+
+rm -rf ./build
+packagesbuild -v ./fplog.pkgproj
+cp ./build/fplog.pkg ../../Stage/
+
---
+++ fplog_testapp/fplog_testapp/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  fplog_testapp
+//
+//  Created by Rostislav Kuratch on 18/02/17.
+//  Copyright © 2017 Rostislav Kuratch. All rights reserved.
+//
+
+#include <iostream>
+#include <fplog/fplog.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
