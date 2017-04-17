d3588746c84c9b41cbcc6a3ae783eae4da0c0958
kr0st <kr0st@bioreactor.me>
2017-04-17 19:46:44 +0300

    Linux autobuild optimizations: boost libs are now built without version info.

--- autobuild/linux/make_dep.sh
+++ autobuild/linux/make_dep.sh
@@ -71,7 +71,8 @@ fi
 rm ./project-config.jam
 cp ../../linux/project-config.jam ./
 
-./b2 -j4 --build-type=complete --layout=versioned --toolset=gcc architecture=x86 address-model=32
+./b2 -j4 --variant=debug --link=shared --layout=tagged --toolset=gcc architecture=x86 address-model=32
+./b2 -j4 --variant=release --link=shared --layout=tagged --toolset=gcc architecture=x86 address-model=32
 
 if [ $? -ne 0 ]; then
 echo "****************************************** ERROR ******************************************"
@@ -80,7 +81,8 @@ echo "**************************************************************************
 exit
 fi
 
-./b2 -j4 --with-python --build-type=complete --layout=versioned --toolset=gcc architecture=x86 address-model=32
+./b2 -j4 --with-python --variant=release --link=shared --layout=tagged --toolset=gcc architecture=x86 address-model=32
+./b2 -j4 --with-python --variant=debug --link=shared --layout=tagged --toolset=gcc architecture=x86 address-model=32
 
 if [ $? -ne 0 ]; then
 echo "****************************************** ERROR ******************************************"
@@ -104,7 +106,8 @@ mv ../../../boost/stage/lib/lib ../../../boost/stage/lib/x86
 rm -rf ./stage
 rm -rf ./bin.v2
 
-./b2 -j4 --build-type=complete --layout=versioned --toolset=gcc address-model=64
+./b2 -j4 --variant=debug --link=shared --layout=tagged --toolset=gcc address-model=64
+./b2 -j4 --variant=release --link=shared --layout=tagged --toolset=gcc address-model=64
 
 if [ $? -ne 0 ]; then
 echo "****************************************** ERROR ******************************************"
@@ -113,7 +116,8 @@ echo "**************************************************************************
 exit
 fi
 
-./b2 -j4 --with-python --build-type=complete --layout=versioned --toolset=gcc address-model=64
+./b2 -j4 --with-python --variant=release --link=shared --layout=tagged --toolset=gcc address-model=64
+./b2 -j4 --with-python --variant=debug --link=shared --layout=tagged --toolset=gcc address-model=64
 
 if [ $? -ne 0 ]; then
 echo "****************************************** ERROR ******************************************"
@@ -134,10 +138,10 @@ mkdir ../../../mongo/lib
 
 mydir="$(pwd)"
 
-scons install --32 --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x86 --boost-lib-search-suffixes=-gcc48-1_63,-gcc48-mt-1_63,-gcc48-mt-d-1_63,-gcc48-d-1_63,-gcc48-s-1_63,-gcc48-sd-1_63,-gcc48-mt-s-1_63,-gcc48-mt-sd-1_63
-scons install --32 --dbg=on --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x86 --boost-lib-search-suffixes=-gcc48-1_63,-gcc48-mt-1_63,-gcc48-mt-d-1_63,-gcc48-d-1_63,-gcc48-s-1_63,-gcc48-sd-1_63,-gcc48-mt-s-1_63,-gcc48-mt-sd-1_63
-scons install --32 --sharedclient --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x86 --boost-lib-search-suffixes=-gcc48-1_63,-gcc48-mt-1_63,-gcc48-mt-d-1_63,-gcc48-d-1_63,-gcc48-s-1_63,-gcc48-sd-1_63,-gcc48-mt-s-1_63,-gcc48-mt-sd-1_63
-scons install --32 --sharedclient --dbg=on --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x86 --boost-lib-search-suffixes=-gcc48-1_63,-gcc48-mt-1_63,-gcc48-mt-d-1_63,-gcc48-d-1_63,-gcc48-s-1_63,-gcc48-sd-1_63,-gcc48-mt-s-1_63,-gcc48-mt-sd-1_63
+scons install --32 --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x86
+scons install --32 --dbg=on --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x86
+scons install --32 --sharedclient --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x86
+scons install --32 --sharedclient --dbg=on --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x86
 
 cd ./build
 
@@ -148,10 +152,10 @@ mv ../../../../mongo/lib/lib ../../../../mongo/lib/x86
 
 cd ..
 
-scons install --64 --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x64 --boost-lib-search-suffixes=-gcc48-1_63,-gcc48-mt-1_63,-gcc48-mt-d-1_63,-gcc48-d-1_63,-gcc48-s-1_63,-gcc48-sd-1_63,-gcc48-mt-s-1_63,-gcc48-mt-sd-1_63
-scons install --64 --dbg=on --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x64 --boost-lib-search-suffixes=-gcc48-1_63,-gcc48-mt-1_63,-gcc48-mt-d-1_63,-gcc48-d-1_63,-gcc48-s-1_63,-gcc48-sd-1_63,-gcc48-mt-s-1_63,-gcc48-mt-sd-1_63
-scons install --64 --sharedclient --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x64 --boost-lib-search-suffixes=-gcc48-1_63,-gcc48-mt-1_63,-gcc48-mt-d-1_63,-gcc48-d-1_63,-gcc48-s-1_63,-gcc48-sd-1_63,-gcc48-mt-s-1_63,-gcc48-mt-sd-1_63
-scons install --64 --sharedclient --dbg=on --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x64 --boost-lib-search-suffixes=-gcc48-1_63,-gcc48-mt-1_63,-gcc48-mt-d-1_63,-gcc48-d-1_63,-gcc48-s-1_63,-gcc48-sd-1_63,-gcc48-mt-s-1_63,-gcc48-mt-sd-1_63
+scons install --64 --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x64
+scons install --64 --dbg=on --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x64
+scons install --64 --sharedclient --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x64
+scons install --64 --sharedclient --dbg=on --cpppath=$mydir/../../../boost --libpath=$mydir/../../../boost/stage/lib/x64
 
 if [ $? -eq 0 ]; then
 echo "*******************************************************************************************"
