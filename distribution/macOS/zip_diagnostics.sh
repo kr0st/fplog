3ff550b9c93ffd48c15c7ca22dca57fe84e33740
kr0st <kr0st@bioreactor.me>
2017-09-02 23:27:52 +0300

    Added diagnostic files to Jenkins release (macOS), issue #80

---
+++ distribution/macOS/zip_diagnostics.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+7za > /dev/null
+if [ $? -ne 0 ]; then
+echo "********************* ERROR *********************"
+echo "Please install 7zip (p7zip-full package)"
+echo "*************************************************"
+exit
+fi
+
+7za a -r fplog_diagnostics.7z ../../Stage/*.txt
+7za a -r fplog_diagnostics.7z ../../Stage/*.xml
+7za a -r fplog_diagnostics.7z ../../Stage/*.log
+
+rm -rf ../../Stage/fplog_diagnostics*
+
+mv ./fplog_diagnostics.7z ../../Stage
+
